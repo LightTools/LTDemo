b5ed92011ade456377a69ab600eadf9cd427bfa0
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2018-09-14 23:02:32 +0300

    Updated Demo application.

---
+++ src/aura/demoScroller/demoScrollerController.js
@@ -0,0 +1,34 @@
+({
+    initialize : function(component, event, helper) {
+        helper.setDefaults(component);
+    },
+    handleScroll : function(component, event) {
+        let eventData = event.getParam("data");
+        // set position values
+        component.set("v.position", {
+            "horizontal": eventData.target.scrollLeft,
+            "horizontalOffset": eventData.target.scrollLeft + eventData.target.clientWidth,
+            "vertical": eventData.target.scrollTop,
+            "verticalOffset": eventData.target.scrollTop + eventData.target.clientHeight
+        });
+        // set scroll size
+        component.set("v.size", component.find("scroller").getSize());
+    },
+    setPosition : function(component, event) {
+        let scroller = component.find("scroller");
+        switch (event.getSource().get("v.value")) {
+            case "horizontalStart":
+                scroller.setPosition({ "horizontal": "start" });
+                break;
+            case "horizontalEnd":
+                scroller.setPosition({ "horizontal": "end" })
+                break;
+            case "verticalStart":
+                scroller.setPosition({ "vertical": "start" })
+                break;
+            case "verticalEnd":
+                scroller.setPosition({ "vertical": "end" })
+                break;
+        }
+    }
+})
